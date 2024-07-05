01e7b7acaa9f63e47920ec45f4f1d987ef01c1ce
Alisher <dautovalisher33@gmail.com>
2024-07-05 23:01:44 +0500

    Add HttpResponse in home

--- crm/views.py
+++ crm/views.py
@@ -1,9 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def homepage(request):
-
-    pass
+    return HttpResponse('this is home page')
 
 def register(request):
 

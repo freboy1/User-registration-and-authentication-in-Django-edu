0455800a9a5dfa929d557fa12543868bfd99e9e4
Alisher <dautovalisher33@gmail.com>
2024-07-09 17:10:26 +0500

    Restricted dashboard page

--- crm/views.py
+++ crm/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect, render
 from .forms import CreateUserForm, LoginForm
 from django.contrib.auth.models import auth
 from django.contrib.auth import login, logout, authenticate
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def homepage(request):
@@ -57,7 +57,7 @@ def user_logout(request):
     logout(request)
     return redirect('')
 
-
+@login_required(login_url='my-login')
 def dashboard(request):
 
     return render(request, 'crm/dashboard.html')
